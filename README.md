48b76dfe5c8ae62f46d0bcbda22b271fa6d61bde
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2016-07-09 02:20:53 +0100

    Start up script to set up Centos 7.  Begin to prepare for Python 3 or Python 2 application

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ctm-deploy
-Deployment scripts for CTM Cenots installation
+Deployment scripts for CTM Centos 7 installation
---
+++ deploy.sh
@@ -0,0 +1,36 @@
+# On lineone Centos & image
+
+# Need to install ibjpeg and zlib Pillow
+yum update
+hostnamectl set-hostname ctm-web
+more /etc/hosts
+timedatectl set-timezone 'Europe/London'
+date
+yum install -y yum-cron
+sed -i.bak 's|apply_updates = no|apply_updates = yes|' /etc/yum/yum-cron.conf
+echo "Set yum to autoupdate"
+# Run below , not sure required
+# rpm -Uvh https://www.softwarecollections.org/en/scls/rhscl/python33/epel-7-x86_64/download/rhscl-python33-epel-7-x86_64.noarch.rpm
+yum install -y epel-release
+yum install -y python34
+yum install -y python34u-wheel
+yum install -y python-pip
+yum install -y git
+yum install -y python-imaging
+yum install -y python-psycopg2
+yum install -y python33-python-psycopg2
+yum install -y fail2ban
+yum install -y sendmail
+systemctl start fail2ban
+systemctl enable fail2ban
+systemctl start sendmail
+systemctl enable sendmail
+
+pip install virtualenv
+
+echo "UPDATE: Replace example_user with actual username"
+useradd example_user && passwd example_user
+usermod -aG wheel example_user
+
+echo "Need to install https://pypi.python.org/pypi/psycopg2"
+
